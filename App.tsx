414627a8051ea4d0c1fc8e3a3e4f83f1c14bbcb4
Ilkvms5 <165666036+Ilkvms5@users.noreply.github.com>
2024-11-20 15:33:44 +0200

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import Starters from './screens/starters'; // Adjust the import paths accordingl
 import MainPage from './screens/main_page'; // Adjust the import paths accordingly
 import Dessert from './screens/dessert';
 import Login from './screens/login';
-import ChefEdit from './chefedit';
+import ChefEdit from './screens/chefedit';
 
 // Define the types for your stack
 type RootStackParamList = {
@@ -17,8 +17,7 @@ type RootStackParamList = {
   Starters: undefined;
   Dessert: undefined; 
   Login: undefined;
-  ChefEdit: undefined; // Add Login to your stack param list
-
+  ChefEdit: undefined; 
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
